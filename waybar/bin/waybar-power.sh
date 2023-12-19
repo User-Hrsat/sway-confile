f6ec1b04367a7aeb1c7e3fb7b70a59afe590065b
User-Hrsat <38604393+User-Hrsat@users.noreply.github.com>
2023-12-20 02:01:10 +0800

    ++

--- waybar/bin/waybar-power.sh
+++ waybar/bin/waybar-power.sh
@@ -1,2 +1,35 @@
-#!/bin/sh
-swaynag -t custom -m 'Are you sure you want to shutdown your device?' -b 'Poweroff' 'poweroff'
+#!/bin/bash
+
+# Define menu options
+options=("Log Out" "Shutdown" "Restart" "Suspend" "Hibernate" "Lock Screen" "System Info"s)
+
+# Prompt user to select an option
+selected_option=$(printf '%s\n' "${options[@]}" | wofi --dmenu --prompt "Custom Menu" | awk '{print $1}')
+
+# Execute corresponding action based on the selected option
+case $selected_option in
+  "Log Out")
+    swaymsg exit
+    ;;
+  "Shutdown")
+    systemctl poweroff
+    ;;
+  "Restart")
+    systemctl reboot
+    ;;
+  "Suspend")
+    systemctl suspend
+    ;;
+  "Hibernate")
+    systemctl hibernate
+    ;;
+  "Lock Screen")
+    swaylock
+    ;;
+  "System Info")
+    kitty -e neofetch
+    ;;
+  *)
+    # Handle unexpected input
+    ;;
+esac
